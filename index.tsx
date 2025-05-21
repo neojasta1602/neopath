8e36b02ae7f906760a5f8012eb8642afd69826d6
neojasta1602 <151819748+neojasta1602@users.noreply.github.com>
2025-05-22 01:47:00 +0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,1 +1,10 @@
+import LifePathSimulator from '../components/LifePathSimulator'
+
+export default function Home() {
+  return (
+    <main>
+      <LifePathSimulator />
+    </main>
+  )
+}
 // Placeholder for index.tsx
